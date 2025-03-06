64a24bd00b55acd0983af3462ef932abea8dff28
Arham Khan <sunnygeegee@gmail.com>
2025-03-06 09:26:52 -0800

    Revert "Merge branch 'main' of https://github.com/akhan-94/tax-calculator"
    This reverts commit 307f853cd931c47a5272d5b86efc89057d19a8a8, reversing
    changes made to 858e3953faea3b150431e158f2c4e196219cd283.

--- src/components/app-footer/_components/copyright.tsx
+++
@@ -1,6 +0,0 @@
-import { getCurrentYear } from "@/lib/date/get-current-year";
-
-export function Copyright() {
-  const currentYear = getCurrentYear();
-  return <p>&copy; {currentYear} Tax Calculator</p>;
-}
